cbd839d4c100aa1f89479c8131471ef6ebb228db
Min Kevin <58409670+MinKevin@users.noreply.github.com>
2023-10-21 14:27:01 +0900

    init

---
+++ src/main/java/com/ebnatural/EbnaturalApplication.java
@@ -0,0 +1,13 @@
+package com.ebnatural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbnaturalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbnaturalApplication.class, args);
+	}
+
+}
